f1ee1ff72911eb2c2e627c399c92a1018d91f6cd
Karandeep Singh <karandeepjabbal@gmail.com>
2022-05-14 21:59:07 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const app = express()
 const Port = 3000 || process.env.PORT
+const cors = require("cors");
+
 
 var UserController = require('./users/UserController');
 var AuthUserController = require('./users/userAuth');
+app.use(cors());
 app.use('/users', UserController);
 app.use('/auth', AuthUserController);
+
 
 app.get("/status",(req,res)=>{
     res.send("Healthy");
