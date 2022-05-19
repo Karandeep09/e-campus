f183ff00ced280b92c1a5d2465ffb4a771e11a78
Karandeep Singh <karandeepjabbal@gmail.com>
2022-05-19 10:38:50 +0530

    auth blog post

--- Frontend/src/sevices/authHeader.service.js
+++ Frontend/src/sevices/authHeader.service.js
@@ -2,8 +2,8 @@ export default function authHeader() {
     const user = JSON.parse(localStorage.getItem('user'));
     if (user && user.token) {
       console.log(user);
-      return { 'x-access-token': user.token };
+      return { 'x-access-token': user.token, 'Content-Type': 'application/json' };
     } else {
-      return {};
+      return {'Content-Type': 'application/json'};
     }
   }
